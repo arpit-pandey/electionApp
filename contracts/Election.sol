0e02ac3ebf5a4aac13dd3dd5097f18c76341bdcf
arpit-pandey <arpitprince1999@gmail.com>
2021-04-21 17:33:23 +0530

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    //Store candiate
+    //Read candiate
+    string public candidate;
+
+    //Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
